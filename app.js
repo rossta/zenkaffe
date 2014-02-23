7894c604e21b57a959184ea3e9fd0776de4963f6
Ross Kaffenberger <rosskaff@gmail.com>
2014-02-23 13:01:24 -0500

    protect post and destroy endpoints with basic digest

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ var errors = require('./errors');
 var http = require('http');
 var path = require('path');
 
+var passport = require('passport');
+var DigestStrategy = require('passport-http').DigestStrategy;
+
 var app = express();
 
 // all environments
@@ -24,11 +27,26 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(express.bodyParser());
+app.use(passport.initialize());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(errors.log);
 app.use(errors.client);
 app.use(errors.server);
+
+passport.use(new DigestStrategy({ qop: 'auth' },
+  function(username, done) {
+    if (username == process.env.USERNAME) {
+      return done(null, username, process.env.PASSWORD);
+    } else {
+      return done(null, false);
+    }
+  },
+  function(params, done) {
+    // validate nonces as necessary
+    done(null, true)
+  }
+));
 
 // development only
 if ('development' == app.get('env')) {
@@ -42,8 +60,8 @@ app.get('/users', user.list);
 app.get('/says', quotes.random);
 app.get('/quotes', quotes.index);
 app.get('/quotes/:id', quotes.show);
-app.post('/quotes', quotes.create);
-app.delete('/quotes/:id', quotes.destroy);
+app.post('/quotes', passport.authenticate('digest', { session: false }), quotes.create);
+app.delete('/quotes/:id', passport.authenticate('digest', { session: false }), quotes.destroy);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
